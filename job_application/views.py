92e8501ed2451a33ca010a70b2525eb1528228af
Anik <mirajulislamanik100@gmail.com>
2025-09-24 02:30:31 +0600

    Update

--- job_application/views.py
+++ job_application/views.py
@@ -1,28 +1,44 @@
 from django.shortcuts import render, redirect
+from django.db import IntegrityError
 from django.contrib import messages
 from .forms import ApplicationForm
+from .models import Form
 
 def index(request):
-    if request.method == "POST":
-        form = ApplicationForm(request.POST)
-        if form.is_valid():
-            data = form.cleaned_data
-            first_name = data.get("first_name")
-            last_name = data.get("last_name" )
-            email = data.get("email")
-            phone = data.get("phone")
-            position = data.get("position")
-            experience = data.get("experience")
-            availability = data.get("availability")
-            terms = data.get("terms")
-            newsletter = data.get("newsletter")
+    try :
+        if request.method == "POST":
+            form = ApplicationForm(request.POST)
+            if form.is_valid():
+                data = form.cleaned_data
+                first_name = data.get("first_name")
+                last_name = data.get("last_name" )
+                email = data.get("email")
+                phone = data.get("phone")
+                position = data.get("position")
+                experience = data.get("experience" , "")
+                availability = data.get("availability" , None)
+                terms = data.get("terms")
+                newsletter = data.get("newsletter")
 
-            messages.success(request, "Application submitted successfully!")
-            return redirect('index')  # Redirect to avoid form resubmission
+                Form.objects.create(first_name= first_name,
+                                    last_name= last_name,
+                                    email= email,
+                                    phone= phone,
+                                    position= position,
+                                    experience= experience,
+                                    availability= availability,
+                                    terms = terms,
+                                    newsletter = newsletter)
+
+                messages.success(request, "Application submitted successfully!")
+                return redirect('index')  # Redirect to avoid form resubmission
+            else:
+                messages.error(request, "Please enter all the requirement")
+                return redirect('index')
         else:
-            messages.error(request, "Please enter all the requirement")
-            return redirect('index')
-    else:
-        form = ApplicationForm()  # Initialize form for GET request
+            form = ApplicationForm()  # Initialize form for GET request
+    except IntegrityError:
+        messages.error(request, "You have already submitted this form.")
+        return redirect('index')
 
-    return render(request, "index.html" , {"form" : form})
+    return render(request, "index.html", {"form": form})

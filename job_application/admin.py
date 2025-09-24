cc0ed55e7107400189ed2a8014dc55bdc337bc48
Anik <mirajulislamanik100@gmail.com>
2025-09-25 03:49:39 +0600

    upadated

--- job_application/admin.py
+++ job_application/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Form
 
-# Register your models here.
+class AdminForm(admin.ModelAdmin):
+    list_display = ("first_name" , "last_name" , "email" , "phone")
+    search_fields = ("first_name" , "last_name" , "email" , "phone")
+    list_filter = ("availability"  , "position")
+    ordering = ("first_name",)
+    readonly_fields = ("position" ,)
+    list_display_links = ("first_name" ,)
+
+admin.site.register(Form , AdminForm)
